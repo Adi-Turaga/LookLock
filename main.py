a3da3480c16d6ef7d35257ed65930e8c264745c2
Adi Turaga <adi.s.turaga@gmail.com>
2024-08-20 15:54:36 -0400

    Added name==main statement

--- main.py
+++ main.py
@@ -14,29 +14,30 @@ num_pics = 6
 
 vid = cv2.VideoCapture(channel)
 
-while True:
+if __name__ == "__main__":
     while True:
-        ret, frame = vid.read()
-        cv2.imshow('Webcam feed', frame)
+        while True:
+            ret, frame = vid.read()
+            cv2.imshow('Webcam feed', frame)
 
-        if cv2.waitKey(1) & 0xFF == ord('r'):
-            img_name = Recognizer.gen_pic_name()
-            cv2.imwrite(img_name, frame)
-            try:
-                result, name = Recognizer.recognize_face(img_name, encodings_file)
-                if result == True:
-                    print("Recognized {}".format(name))
-            except Exception as e:
-                print("[ERORR] An error occured: {}".format(e))
-                os.remove(img_name)
+            if cv2.waitKey(1) & 0xFF == ord('r'):
+                img_name = Recognizer.gen_pic_name()
+                cv2.imwrite(img_name, frame)
+                try:
+                    result, name = Recognizer.recognize_face(img_name, encodings_file)
+                    if result == True:
+                        print("Recognized {}".format(name))
+                except Exception as e:
+                    print("[ERORR] An error occured: {}".format(e))
+                    os.remove(img_name)
 
-        elif cv2.waitKey(1) & 0xFF == ord('e'):
-            name = Gui.prompt_name()
-            encoder = Encoder(vid, encodings_file, faces_db, name)
-            encoder.create_dir_if_needed()
-            encoder.encode_face()
-        
-        elif cv2.waitKey(1) & 0xFF == ord('q'):
-            cv2.destroyAllWindows()
-            print("Program terminated...have a great day")
-            exit()
+            elif cv2.waitKey(1) & 0xFF == ord('e'):
+                name = Gui.prompt_name()
+                encoder = Encoder(vid, encodings_file, faces_db, name)
+                encoder.create_dir_if_needed()
+                encoder.encode_face()
+            
+            elif cv2.waitKey(1) & 0xFF == ord('q'):
+                cv2.destroyAllWindows()
+                print("Program terminated...have a great day")
+                exit()
